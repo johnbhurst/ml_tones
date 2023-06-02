14e553dc6a8688ca02c5f1f8bf8f7718292ddc8b
John Hurst <john.b.hurst@gmail.com>
2023-06-02 15:09:46 +1200

    feat: Use command-line arg as file; better output filename.

--- split_audio.py
+++ split_audio.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+
 import numpy as np
+import sys
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 
+
 # Load your audio file
-audio_file = "path/to/your/audio/file.wav"
+audio_file = sys.argv[1]
+base_filename = audio_file.split('.')[0]
 audio = AudioSegment.from_wav(audio_file)
 
 # Convert audio to numpy array (needed for scipy)
@@ -16,8 +21,8 @@ elif audio.sample_width == 4:
     samples = np.frombuffer(samples, dtype=np.int32)
 
 # Where the clip is silent, the sample value is zero. So we split the audio where sample is zero
-chunks = split_on_silence(audio, min_silence_len=100, silence_thresh=-32)
+chunks = split_on_silence(audio, min_silence_len=100, silence_thresh=-64)
 
 # Save each chunk to separate files
 for i, chunk in enumerate(chunks):
-    chunk.export(f"output_chunk_{i}.wav", format="wav")
+    chunk.export(f"{base_filename}_{i}.wav", format="wav")
