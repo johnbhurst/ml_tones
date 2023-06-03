f5e82e6401a5d0c44c172a7a2b29e9ac482d2ca3
John Hurst <john.b.hurst@gmail.com>
2023-06-04 10:29:21 +1200

    feat: Added --min_duration argument.

--- bin/split_tones.py
+++ bin/split_tones.py
@@ -9,12 +9,14 @@ from pydub.silence import split_on_silence
 
 parser = argparse.ArgumentParser(description='Split audio file into chunks based on silence')
 parser.add_argument('--keep_silence', type=int, default=100, help='Keep this much silence at the beginning and end of each chunk (in ms)')
+parser.add_argument('--min_duration', type=float, default=0.5, help='Minimum duration of each chunk (in seconds)')
 parser.add_argument('--min_silence_len', type=int, default=100, help='Minimum length of silence to split on (in ms)')
 parser.add_argument('--silence_thresh', type=int, default=-64, help='Threshold of silence to split on (in dBFS)')
 parser.add_argument('audio_files', nargs=argparse.REMAINDER, help='Audio files to split')
 args = parser.parse_args()
 
 keep_silence = args.keep_silence
+min_duration = args.min_duration
 min_silence_len = args.min_silence_len
 silence_thresh = args.silence_thresh
 audio_files = args.audio_files
@@ -33,19 +35,19 @@ for audio_file in audio_files:
         samples = np.frombuffer(samples, dtype=np.int32)
 
     chunks = split_on_silence(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh, keep_silence=keep_silence)
-
+    chunk_no = 1
     for i, chunk in enumerate(chunks):
-        # print(f"Chunk {i+1} duration: {chunk.duration_seconds:.2f} seconds")
-        chunk.export(f"{base_filename}/{i+1}.wav", format="wav")
+        if chunk.duration_seconds > min_duration:
+            chunk.export(f"{base_filename}/{chunk_no}.wav", format="wav")
 
-        chunk_samples = np.array(chunk.get_array_of_samples())
-        N = len(chunk_samples)
-        yf = rfft(chunk_samples)
-        xf = rfftfreq(N, 1 / audio.frame_rate)
+            chunk_samples = np.array(chunk.get_array_of_samples())
+            N = len(chunk_samples)
+            yf = rfft(chunk_samples)
+            xf = rfftfreq(N, 1 / audio.frame_rate)
 
-        # Find the peak frequency
-        idx = np.argmax(np.abs(yf))
-        freq = xf[idx]
+            # Find the peak frequency
+            idx = np.argmax(np.abs(yf))
+            freq = xf[idx]
 
-        # print(f"Chunk {i} duration: {chunk.duration_seconds:.2f} seconds, primary frequency: {freq:.2f} Hz")
-        print(f"{i},{chunk.duration_seconds:.2f},{freq:.2f}")
+            print(f"{chunk_no},{chunk.duration_seconds:.2f},{freq:.2f}")
+            chunk_no += 1
