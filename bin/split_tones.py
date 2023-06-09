45ac60d7f2fb022b761a8290eb1fe964fa1af84b
John Hurst <john.b.hurst@gmail.com>
2023-06-10 10:03:33 +1200

    feat: Added --save-dir option to split_tones.py.

--- bin/split_tones.py
+++ bin/split_tones.py
@@ -14,6 +14,7 @@ parser.add_argument('--keep-silence', type=int, default=100, help='Keep this muc
 parser.add_argument('--min-duration', type=float, default=0.5, help='Minimum duration of each chunk (in seconds)')
 parser.add_argument('--min-silence-len', type=int, default=100, help='Minimum length of silence to split on (in ms)')
 parser.add_argument('--silence-thresh', type=int, default=-64, help='Threshold of silence to split on (in dBFS)')
+parser.add_argument('--save-dir', type=str, default='data/raw_tones', help='Directory to save chunks to')
 parser.add_argument('audio_files', nargs=argparse.REMAINDER, help='Audio files to split')
 args = parser.parse_args()
 
@@ -24,14 +25,14 @@ for audio_file in args.audio_files:
     min_silence_len = conf[0].get('min_silence_len', args.min_silence_len)
     silence_thresh = conf[0].get('silence_thresh', args.silence_thresh)
 
-    base_filename = audio_file.split('.')[0]
-    if not os.path.isdir(base_filename):
-        os.mkdir(base_filename)
+    save_path = args.save_dir + "/" + os.path.basename(audio_file.split('.')[0])
+    if not os.path.isdir(save_path):
+        os.makedirs(save_path)
 
     audio = AudioSegment.from_wav(audio_file)
     chunks = split_on_silence(audio, min_silence_len=min_silence_len, silence_thresh=silence_thresh, keep_silence=keep_silence)
     midi_no = 36
     for i, chunk in enumerate(chunks):
         if chunk.duration_seconds > min_duration:
-            chunk.export(f"{base_filename}/{midi_no}.wav", format="wav")
+            chunk.export(f"{save_path}/{midi_no}.wav", format="wav")
             midi_no += 1
